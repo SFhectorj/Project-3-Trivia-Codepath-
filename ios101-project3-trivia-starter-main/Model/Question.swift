41f80482392fb7a8aebc206932bb8db140b2794c
Hector B <108562790+SFhectorj@users.noreply.github.com>
2024-03-11 22:54:49 -0700

    Add files via upload

---
+++ ios101-project3-trivia-starter-main/Model/Question.swift
@@ -0,0 +1,17 @@
+//
+//  Question.swift
+//  Trivia
+//
+//  Created by Hector J. Baeza on 3/11/24.
+//
+
+import Foundation
+import UIKit
+
+struct Question {
+    let count: Int
+    let genre: String
+    let text: String
+    let answer: String
+    let color: UIColor
+}
